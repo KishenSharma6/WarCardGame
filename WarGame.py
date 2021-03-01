fb0f539c2ba8262f7d9ba55b3b1ee038d91f8f41
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-02-28 18:04:50 -0800

    Minor edits, will create seperate sandbox py file

--- WarGame.py
+++ WarGame.py
@@ -1,12 +1,13 @@
 #Read in classes
 import os
-os.chdir('/Users/ksharma/Documents/Data Science/Software Engineering/Object Oriented Programming/OOP Projects/')
+os.chdir('/Users/ksharma/Documents/Data Science/Software Engineering/Object Oriented Programming/OOP Projects/Card Game/')
+
 
 from WarClasses import Card, Deck, Player
 
 
 deck1= Deck()
-deck1.show()
+deck1.draw_card().show()
 deck1.shuffle()
 deck1.show()
 deck1.draw_card(deck1)
@@ -16,9 +17,13 @@ player1= Player('Jane', 3000)
 player2= Player('Joe', 5000)
 player1.place_bet(2000)
 player2.place_bet(2000)
+deck1.draw_card().show()
 player1.card= deck1.draw_card()
-player1.__dict__
-player2.__dict__
+player2.card= deck1.draw_card()
+player1.card.show()
+player2.card.show()
+player1.a
 # player2= Player('Bob', 4000)
 # player2.draw(deck1)
 # player2.card
+deck1.draw_card()
