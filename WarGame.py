7574e9bd760b328d2c8872a18f601a333d668cc6
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-03-05 09:05:11 -0800

    Removed messy code, moved to SandBox

--- WarGame.py
+++ WarGame.py
@@ -1,29 +1,5 @@
 #Read in classes
 import os
-os.chdir('/Users/ksharma/Documents/Data Science/Software Engineering/Object Oriented Programming/OOP Projects/Card Game/')
-
+os.chdir('/Users/ksharma/Documents/ML Engineer/Software Engineering/Python/Object Oriented Programming/OOP Projects/Card Game/')
 
 from WarClasses import Card, Deck, Player
-
-
-deck1= Deck()
-deck1.draw_card().show()
-deck1.shuffle()
-deck1.show()
-deck1.draw_card(deck1)
-
-deck1.cards[0]
-player1= Player('Jane', 3000)
-player2= Player('Joe', 5000)
-player1.place_bet(2000)
-player2.place_bet(2000)
-deck1.draw_card().show()
-player1.card= deck1.draw_card()
-player2.card= deck1.draw_card()
-player1.card.show()
-player2.card.show()
-player1.a
-# player2= Player('Bob', 4000)
-# player2.draw(deck1)
-# player2.card
-deck1.draw_card()
